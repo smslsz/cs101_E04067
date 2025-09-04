ac2812c6b98e134af93d634ae7a2a3cb0d8f01d1
smslsz <2138799740lsz@gmail.com>
2025-09-04 14:45:51 +0800

    添加项目文件。

---
+++ cs101_E04067.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+int main() {
+    while (true) {
+        vector<int> digits;
+        string s;
+        cin >> s;
+        istringstream iss(s);
+        char c;
+        while (iss >> c) {
+            if (isdigit(c)) {
+                digits.push_back(c - '0');
+            }
+        }
+        vector<int> reversedigits;
+        reversedigits = digits;
+        reverse(reversedigits.begin(), reversedigits.end());
+        if (reversedigits == digits) { cout << "YES" << endl; }
+        else { cout << "NO" << endl; }
+    }
+}
